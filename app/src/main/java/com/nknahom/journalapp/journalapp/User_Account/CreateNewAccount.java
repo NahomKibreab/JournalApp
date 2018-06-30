ab3edf8c190846e2b2dbcc22a315b48cdba2e480
Nahom Kibreab <nknahom@gmail.com>
2018-06-30 17:38:21 +0300

    Connected to Firebase Server and Create User Account

---
+++ app/src/main/java/com/nknahom/journalapp/journalapp/User_Account/CreateNewAccount.java
@@ -0,0 +1,128 @@
+package com.nknahom.journalapp.journalapp.User_Account;
+
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.nknahom.journalapp.journalapp.MainActivity;
+import com.nknahom.journalapp.journalapp.R;
+
+public class CreateNewAccount extends AppCompatActivity {
+
+    private Button btnReg;
+    private EditText inName, inEmail, inPass;
+
+    private FirebaseAuth fAuth;
+    private DatabaseReference fUsersDatabase;
+
+    private ProgressDialog progressDialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.create_new_account);
+
+        btnReg = findViewById(R.id.btn_reg);
+        inName = (EditText) findViewById(R.id.input_reg_name);
+        inEmail = (EditText) findViewById(R.id.input_reg_email);
+        inPass = (EditText) findViewById(R.id.input_reg_pass);
+
+        fAuth = FirebaseAuth.getInstance();
+        fUsersDatabase = FirebaseDatabase.getInstance().getReference().child("Users");
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+
+        btnReg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String uname = inName.getText().toString().trim();
+                String uemail = inEmail.getText().toString().trim();
+                String upass = inPass.getText().toString().trim();
+
+                registerUser(uname, uemail, upass);
+
+            }
+        });
+
+    }
+
+    private void registerUser(final String name, String email, String password) {
+
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Processing your request, please wait...");
+
+        progressDialog.show();
+
+        fAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if (task.isSuccessful()) {
+
+                            fUsersDatabase.child(fAuth.getCurrentUser().getUid())
+                                    .child("basic").child("name").setValue(name)
+                                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<Void> task) {
+
+                                            if (task.isSuccessful()) {
+
+                                                progressDialog.dismiss();
+
+                                                Intent mainIntent = new Intent(CreateNewAccount.this, MainActivity.class);
+                                                startActivity(mainIntent);
+                                                finish();
+                                                Toast.makeText(CreateNewAccount.this, "User created!", Toast.LENGTH_SHORT).show();
+
+                                            } else {
+                                                progressDialog.dismiss();
+                                                Toast.makeText(CreateNewAccount.this, "ERROR : " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                                            }
+
+                                        }
+                                    });
+
+                        } else {
+
+                            progressDialog.dismiss();
+
+                            Toast.makeText(CreateNewAccount.this, "ERROR: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+
+                        }
+
+                    }
+                });
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        super.onOptionsItemSelected(item);
+
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                break;
+        }
+
+        return true;
+    }
+
+}
